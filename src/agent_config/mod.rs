e7649455305c4d427845562c0d645d06355fc1a6
Ivan Kuchin <ivan.kuchin@gmail.com>
2026-02-16 23:34:43 -0500

    minor change

--- src/agent_config/mod.rs
+++ src/agent_config/mod.rs
@@ -2,8 +2,10 @@
 //! - Agent self-registration and claiming workflow
 
 mod loader;
-pub mod server_config;
+mod reload;
+mod server_config;
 mod types;
 
+pub use reload::{handle_sighup, load_and_validate_config, ConfigReloadManager, ReloadTrigger};
 pub use server_config::{ClaimConfig, ServerConfig};
 pub use types::*;
