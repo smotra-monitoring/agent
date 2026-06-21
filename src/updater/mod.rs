b2439dbe4911e1a61ebc4b4812fd4bb0450f8798
Ivan Kuchin <ivan.kuchin@gmail.com>
2026-06-21 18:56:46 -0400

    Feature: self-upgrade: for unification tokio::spwn file renamed to server.rs

--- src/updater/mod.rs
+++ src/updater/mod.rs
@@ -2,11 +2,11 @@ mod checker;
 mod downloader;
 mod environment;
 mod replacer;
-mod task;
+mod server;
 
 use checker::is_newer_than_current;
 
 pub use checker::fetch_latest_version;
 pub use downloader::download_release_binary;
 pub use replacer::replace_binary_and_restart;
-pub use task::run_update_checker;
+pub use server::run_update_checker;
